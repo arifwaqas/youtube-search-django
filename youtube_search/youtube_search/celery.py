0cf5e366562506f2b1f274778c796fa39d8d53ad
arifwaqas <arifw.cs.18@nitj.ac.in>
2021-11-20 08:51:23 +0300

    Initialized django project

---
+++ youtube_search/youtube_search/celery.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_search.settings')
+
+app=Celery('youtube_search')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.autodiscover_tasks()
+
+
+
+
+
